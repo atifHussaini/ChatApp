82e8b850cb6f32d7cd8c2fd96c4a289c9f0ff55f
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-06-13 15:13:16 -0500

    basic chat app functionality complete

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,9 +1,18 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import { PrettyChatWindow} from 'react-chat-engine-pretty';
 
-const ChatsPage = () => {
+const ChatsPage = (props) => {
+
+    
     return (
-        <div>
-            chats....
+        <div style={{height: '100vh'}}>
+            <PrettyChatWindow 
+            projectId='65d09dbc-6e04-4019-8a75-7f6b1881efc2'
+            username={props.user.username} 
+            secret={props.user.secret} 
+            style={{ height: '100%' }} 
+            /> 
         </div>
     );
 };
